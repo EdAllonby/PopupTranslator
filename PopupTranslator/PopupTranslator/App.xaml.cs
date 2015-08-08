e3286478f959ee4938d7bdcb0b95066fa7dd3f5b
Ed Allonby <edward.allonby@gmail.com>
2015-08-08 22:48:32 +0100

    Reference NHotkey and add proof of concept

--- PopupTranslator/PopupTranslator/App.xaml.cs
+++ PopupTranslator/PopupTranslator/App.xaml.cs
@@ -1,11 +1,34 @@
 ﻿using System.Windows;
+using System.Windows.Input;
+using NHotkey;
+using NHotkey.Wpf;
 
 namespace PopupTranslator
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App
     {
+        private MainWindow mainWindow;
+
+        private const ModifierKeys ModifierKeys = System.Windows.Input.ModifierKeys.Control | System.Windows.Input.ModifierKeys.Alt;
+        private const Key ActionKey = Key.Down;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            ShutdownMode = ShutdownMode.OnExplicitShutdown;
+
+            HotkeyManager.Current.AddOrReplace("Increment", ActionKey, ModifierKeys, OnIncrement);
+        }
+
+        private void OnIncrement(object o, HotkeyEventArgs e)
+        {
+            if (mainWindow == null || !mainWindow.IsActive)
+            {
+                mainWindow = new MainWindow();
+                mainWindow.Show();
+            }
+        }
     }
 }
